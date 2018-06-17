a0bee9ed7f629ce24b76ccb6297cbeaa5faddb0d
WindAsMe <542636539@qq.com>
2018-06-17 12:40:59 +0800

    PythonForLeeCode

---
+++ venv/include/intersection.py
@@ -0,0 +1,23 @@
+# !/usr/bin/python3
+# -- coding: UTF-8 --
+# Author   :WindAsMe
+# Date     :18-6-17 下午12:17
+# File     :intersection.py
+# Location:/Home/PycharmProjects/..
+
+# LeeCode No.349
+
+
+class Solution:
+    @staticmethod
+    def intersection(nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        return list(set(nums1).intersection(set(nums2)))
+
+
+if __name__ == '__main__':
+    print(Solution.intersection([1,2,2,1], [2,2]))
